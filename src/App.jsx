ada60f4fcb73b45f4d659d85f263b1bf53c1f9cd
daisydazzling768@gmail.com <daisydazzling768@gmail.com>
2025-12-17 21:42:26 +0630

    Updating main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,23 @@
+// import { Routes, Route } from "react-router";
+// import "./App.css";
+// import { HomePage } from "./pages/HomePage";
+// import { Destination } from "./pages/DestinationPage";
+// import { Crew } from "./pages/CrewPage";
+// import { Technology } from "./pages/TechnologyPage";
+
+// function App() {
+//   return (
+//     <Routes>
+//       <Route index element={<HomePage />} />
+//       <Route path="/DestinationPage" element={<Destination />} />
+//       <Route path="/CrewPage" element={<Crew />} />
+//       <Route path="/TechnologyPage" element={<Technology />} />
+//     </Routes>
+//   );
+// }
+
+// export default App;
+// need to fix this
 import { Routes, Route } from "react-router";
 import "./App.css";
 import { HomePage } from "./pages/HomePage";
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Space-tourism-multi-page-website/">
       <App />
     </BrowserRouter>
   </StrictMode>
